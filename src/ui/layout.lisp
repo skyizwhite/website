ef0cd5b77e1ef6981b6cabb98397713597c5d990
paku <paku@skyizwhite.dev>
2024-02-03 13:57:35 +0900

    Use flute instead of markup

--- src/ui/layout.lisp
+++ src/ui/layout.lisp
@@ -1,18 +1,16 @@
 (defpackage #:hp/ui/layout
   (:use #:cl)
-  (:local-nicknames (#:mk #:markup))
+  (:local-nicknames (#:f #:flute))
   (:export #:layout))
 (in-package #:hp/ui/layout)
 
-(markup:enable-reader)
-
-(mk:deftag layout (children)
-  <html>
-    <head>
-      <title>skyizwhite.dev</title>
-      <script src="/static/htmx.min.js" ></script>
-      <link href="/static/main.css" rel="stylesheet">
-      <link href="/static/tailwind.css" rel="stylesheet">
-    </head>
-    <body class="h-[100svh]" >,@(progn children)</body>
-  </html>)
+(f:define-element layout ()
+  (f:h
+    (html
+     (head
+      (title "skyizwhite.dev")
+      (script :src "/static/htmx.min.js")
+      (link :href "/static/main.css" :rel "stylesheet")
+      (link :href "/static/tailwind.css" :rel "stylesheet"))
+     (body :class "h-[100svh]"
+           f:children))))
