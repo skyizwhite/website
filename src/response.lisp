960d9b846857c8b3bbb61dc8ed5542a3828b0a71
paku <paku@skyizwhite.dev>
2024-06-18 10:14:51 +0900

    Upgrade HTMX to v2.0.0

--- src/response.lisp
+++ src/response.lisp
@@ -13,8 +13,8 @@
        (meta :charset "UTF-8")
        (meta :name "viewport" :content "width=device-width, initial-scale=1")
        (link :rel "stylesheet" :href "/dist.css")
-       (script :src "https://cdn.jsdelivr.net/npm/htmx.org@1.9.12/dist/htmx.min.js")
-       (script :src "https://cdn.jsdelivr.net/npm/htmx.org@1.9.12/dist/ext/head-support.js")
+       (script :src "https://cdn.jsdelivr.net/npm/htmx.org@2.0.0/dist/htmx.min.js")
+       (script :src "https://cdn.jsdelivr.net/npm/htmx-ext-head-support@2.0.0/head-support.min.js")
        (script :src "https://cdn.jsdelivr.net/npm/alpinejs@3.14.0/dist/cdn.min.js" :defer t)
        (title (format nil "~@[~a - ~]skyizwhite.dev" title))
        (meta
