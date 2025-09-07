74ac783ad3efd0c5f76a71ed551aad464b1bf14d
Akira Tempaku <paku@skyizwhite.dev>
2025-09-08 01:23:16 +0900

    Improve SP header

--- src/components/header.lisp
+++ src/components/header.lisp
@@ -33,37 +33,35 @@
      :class "flex md:hidden justify-between py-2 border-b-1 top-0 bg-white"
      (p :class "z-20 text-2xl font-bold"
        (a :href "/" "skyizwhite"))
-     (div
+     (div :class "flex justify-end"
        (button
          :aria-label "Open menu"
-         :class "z-20 size-8 flex flex-col justify-center cursor-pointer relative"
+         :class "h-8 cursor-pointer"
          :type "button"
-         :@click "open = !open"
-         (span :class "grid justify-items-center gap-1.5"
-           (span
-             :class "h-1 w-8 rounded-full bg-black transition duration-200"
-             :|:class| "open && 'rotate-45 translate-y-2.5'")
-           (span
-             :class "h-1 w-8 rounded-full bg-black transition duration-200"
-             :|:class| "open && 'scale-x-0'")
-           (span
-             :class "h-1 w-8 rounded-full bg-black transition duration-200"
-             :|:class| "open && '-rotate-45 -translate-y-2.5'")))
+         :@click "open = true"
+         (span :class "font-bold text-lg"
+           "menu"))
        (nav
-         :class (clsx "fixed flex flex-col items-center justify-center"
-                      "z-10 top-0 right-0 w-full h-full gap-16 bg-gray-200")
+         :class "fixed flex flex-col z-10 top-0 right-0 p-2 w-full h-full bg-gray-200"
          :x-show "open"
          :x-transition t
          :|x-transition:enter.duration.200ms| t
          :|x-transition:leave.duration.200ms| t
-         (h2 :class "text-5xl font-bold"
-           "Menu")
-         (ul :class "flex flex-col h-fit gap-8 text-3xl font-bold"
-           (loop
-             :for (href label) :in *sp-menu* :collect
-                (if (string= href (request-uri *request*))
-                    (hsx (li :class "text-pink-500" label))
-                    (hsx (li (a :href href :class "hover:text-pink-500" label)))))))))))
+         (div :class "flex justify-end"
+           (button
+             :aria-label "Close menu"
+             :type "button"
+             :@click "open = false"
+             (img :class "size-8" :src "/assets/img/icon/close.svg")))
+         (div :class "flex flex-col items-center pt-16 gap-16"
+           (h2 :class "text-5xl font-bold"
+             "Menu")
+           (ul :class "flex flex-col h-fit gap-8 text-3xl font-bold"
+             (loop
+               :for (href label) :in *sp-menu* :collect
+                  (if (string= href (request-uri *request*))
+                      (hsx (li :class "text-pink-500" label))
+                      (hsx (li (a :href href :class "hover:text-pink-500" label))))))))))))
 
 (defcomp ~header ()
   (hsx
