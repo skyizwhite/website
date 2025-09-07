d5eee86493ebff04165486b5667fc6aa22a61f3a
Akira Tempaku <paku@skyizwhite.dev>
2025-09-08 01:39:07 +0900

    Change font

--- src/components/scripts.lisp
+++ src/components/scripts.lisp
@@ -4,29 +4,17 @@
   (:export #:~scripts))
 (in-package #:website/components/scripts)
 
-(defparameter *google-font-url*
-  "https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;700&display=swap")
-
 (defun bust-cache (url)
   (format nil "~a?v=~a" url #.(get-universal-time)))
 
 (defcomp ~scripts ()
   (hsx
    (<>
-     (link :rel "stylesheet" :href (bust-cache "/assets/style/dist.css"))
      (link :rel "preconnect" :href "https://fonts.googleapis.com")
      (link :rel "preconnect" :href "https://fonts.gstatic.com" :crossorigin t)
      (link
-       :rel "preload"
-       :as "style"
-       :fetchpriority "high"
-       :href *google-font-url*)
-     (link
        :rel "stylesheet"
-       :href *google-font-url*
-       :media "print"
-       :onload "this.media='all'")
-     (noscript
-       (link :rel "stylesheet" :href *google-font-url*))
+       :href "https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;600&display=swap")
+     (link :rel "stylesheet" :href (bust-cache "/assets/style/dist.css"))
      ;(script :src "https://cdn.jsdelivr.net/npm/htmx.org@2.0.4/dist/htmx.min.js")
      (script :src "https://cdn.jsdelivr.net/npm/alpinejs@3.14.9/dist/cdn.min.js" :defer t))))
